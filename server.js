0729ded66d41dbca19ffc32dad67736058fd5c2c
jorgeenrique94 <jorge.enrique@iconstituent.com>
2024-07-21 23:23:44 -0500

    refactoring

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/ClientApp/dist/client-app'));
+app.use(express.static(__dirname + '/dist/client-app'));
 
 app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+'/ClientApp/dist/client-app/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/client-app/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
